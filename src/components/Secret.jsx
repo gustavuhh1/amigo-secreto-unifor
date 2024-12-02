7dfbcefe24c9b671118a915efb8741fa9ccefa7e
joaooliveiradev1 <joaooliveiradev1@gmail.com>
2024-12-02 13:23:43 -0300

    feat: add secret page

--- src/components/Secret.jsx
+++ src/components/Secret.jsx
@@ -3,8 +3,20 @@ import "./secret.css";
 export function Secret(){
     return(
         <>
-
-        
+        <div id="page">
+            <h2>Sorteando...</h2>
+            <div className="containerReveal">
+                <h3>Gustavo Martins
+                    <hr />
+                </h3>
+                <p>seu amigo secreto é...</p>
+                <button id="reveal" >Revelar</button>
+            </div>
+            <div className="moveButtons">
+                <button>Cancelar</button>
+                <button>Próximo</button>
+            </div>
+        </div>
         </>   
     )
 }
