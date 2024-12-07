e1646eccd0644419a77f51a79d97daae75e85d32
GustavoMartinsR <gustavomrodrigues11@gmail.com>
2024-12-07 15:53:41 -0300

    feat: logica final em progresso

--- src/components/Secret.jsx
+++ src/components/Secret.jsx
@@ -2,29 +2,43 @@ import "./secret.css";
 import { Footer } from "./Footer";
 import { Link } from "react-router";
 
-export function Secret(){
+const secretCamp = (e) => {
+  document.getElementById;
 
-    return (
-      <>
-        <div id="page">
-          <h2>Sorteando...</h2>
-          <div className="containerReveal">
-            <h3>
-              Gustavo Martins
-              <hr />
-            </h3>
-            <p>seu amigo secreto é...</p>
-            <button id="reveal">Revelar</button>
-          </div>
-          <div className="moveButtons">
-            <button>
-              <Link onClick={() => {localStorage.removeItem('resultado')}}to={"/amigo-secreto-unifor/amigos"}>Cancelar</Link>
-            </button>
-            <button>Próximo</button>
-          </div>
+  const listParticipantes = localStorage.getItem("resultado");
+  let count = 0;
+  while (count <= listParticipantes.length) {}
+  e.value.target = "Revelar";
+};
+
+export function Secret() {
+  return (
+    <>
+      <div id="page">
+        <h2>Sorteando...</h2>
+        <div className="containerReveal">
+          <h3>
+            {name}
+            <hr />
+          </h3>
+          <p>seu amigo secreto é...</p>
+          <button id="reveal">{secretCamp}</button>
         </div>
-        <Footer />
-      </>
-    );
+        <div className="moveButtons">
+          <button>
+            <Link
+              onClick={() => {
+                localStorage.removeItem("resultado");
+              }}
+              to={"/amigo-secreto-unifor/amigos"}
+            >
+              Cancelar
+            </Link>
+          </button>
+          <button>Próximo</button>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
 }
-
